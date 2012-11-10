3bf31e7e40b6693e5232d76e441344268d24b136
Martin Maechler <maechler@r-project.org>
2012-11-10 16:22:42 +0100

    rm 2 files that were *NOT* in 1.0-1

--- src/bitOps.h
+++
@@ -1,8 +0,0 @@
-#include <R.h>
-
-void bitFlip(Sint *n, Sint *a, Sint *aflip );
-void bitAnd(Sint *na, Sint *a, Sint *nb, Sint *b, Sint *aAb);
-void bitOr (Sint *na, Sint *a, Sint *nb, Sint *b, Sint *aAb);
-void bitXor(Sint *na, Sint *a, Sint *nb, Sint *b, Sint *aAb);
-void bitShiftL(Sint *na, Sint *a, Sint *nb, Sint *b, Sint *aAb );
-void bitShiftR(Sint *na, Sint *a, Sint *nb, Sint *b, Sint *aAb );
